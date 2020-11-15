894b42ff34cdce9597711a598b82cfe1d5d54047
h05001 <mm10a201@osakac.info>
2020-11-15 18:17:09 +0900

    Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Herokuへのデプロイで以下を追記
+    if (\App::environment('production')) {
+        \URL::forceScheme('https');
+        }
     }
 }
